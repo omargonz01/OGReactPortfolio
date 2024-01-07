3658ab24da8b95f3636b52bcd0eac464a7ec9ffa
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-07 01:19:12 -0800

    add video jsx

--- OG-ReactPortfolio/src/background/Background.jsx
+++ OG-ReactPortfolio/src/background/Background.jsx
@@ -1,2 +1,26 @@
 import React from "react";
-import "../assets/bg-universe.mp4"
+import "../styles/background.css";
+import video from "../assets/bg-universe.mp4";
+import fallbackImage from "../assets/fallback-image.png";
+
+const background = () => {
+    return (
+        <>
+            <div className="shadow-overlay"></div>
+            <video
+            playsInline
+            autoPlay
+            muted
+            loop
+            preload="auto"
+            id="bg"
+            poster={fallbackImage}
+            >
+                <source src={video} type="video?mp4">
+                </source>
+                </video>
+        </>
+    );
+};
+
+export default background;
