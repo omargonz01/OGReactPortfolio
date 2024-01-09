e325f0edc57bfe669d9742235936cbb3d6184fe2
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-08 17:57:57 -0800

    add avatar + initial avatar about stylesheet

--- OG-ReactPortfolio/src/about/About.jsx
+++ OG-ReactPortfolio/src/about/About.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Avatar from "../avatar/Avatar.jsx";
+import AboutMenu from "./AboutMenu.jsx";
 
 export default class About extends Component {
   render() {
     return (
-      <div>About</div>
+      <>
+      <Avatar page="about" />
+      <AboutMenu />
+      </>
     )
   }
 }
