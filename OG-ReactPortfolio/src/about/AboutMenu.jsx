9c1ea694fdb816331721288cf4693ab684669b1e
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-09 16:58:56 -0800

    styling about menu components

--- OG-ReactPortfolio/src/about/AboutMenu.jsx
+++ OG-ReactPortfolio/src/about/AboutMenu.jsx
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import AboutMenuItem from "./AboutMenuItem";
+import AboutSubheading from "./AboutSubheading";
+import subheadingsData from "./subheadingsData";
+import personalIcon from "../assets/moebius-triangle.png"
 
 export default class AboutMenu extends Component {
   render() {
---
+++ OG-ReactPortfolio/src/about/subheadingsData.jsx
@@ -0,0 +1,76 @@
+const subheadingsData = {
+    1: [
+        {
+            title: "About Me",
+            content: (
+                <>
+                    <p>
+                        I am a software engineer with a passion for building
+                        beautiful, responsive, and accessible websites.
+                    </p>
+                </>
+            ),
+        },
+    ],
+    2: [
+        {
+            title: "Coding Temple Software Engineer",
+            content: (
+                <>
+                    <p>
+                        I am a software engineer with a passion for building
+                        beautiful, responsive, and accessible websites.
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "Other Ed",
+            content: (
+                <>
+                    <p>
+                        I am a software engineer with a passion for building
+                        beautiful, responsive, and accessible websites.
+                    </p>
+                </>
+            ),
+        },
+    ],
+    3: [
+        {
+            title: "2023 Frond End Developer",
+            content: (
+                <>
+                    <p>
+                        I am a software engineer with a passion for building
+                        beautiful, responsive, and accessible websites.
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2020 UX Designer",
+            content: (
+                <>
+                    <p>
+                        I am a software engineer with a passion for building
+                        beautiful, responsive, and accessible websites.
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2019 IT Consultant",
+            content: (
+                <>
+                    <p>
+                        I am a software engineer with a passion for building
+                        beautiful, responsive, and accessible websites.
+                    </p>
+                </>
+            ),
+        },
+    ],
+};
+
+export default subheadingsData; 
