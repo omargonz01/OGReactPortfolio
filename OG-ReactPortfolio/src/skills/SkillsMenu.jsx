988f7cc0c3adccb6ad7d390362764312bdb06652
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-12 13:57:26 -0800

    begin building skillsData components

--- OG-ReactPortfolio/src/skills/SkillsMenu.jsx
+++ OG-ReactPortfolio/src/skills/SkillsMenu.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import classNames from "classnames";
 import "../styles/skillsMenu.css";
+import frontendIcon from "../assets/eagle-emblem.png";
+import backendIcon from "assets/hawk-emblem.png";
 
 export default class SkillsMenu extends Component {
   render() {
---
+++ OG-ReactPortfolio/src/skills/skillData.jsx
@@ -0,0 +1,70 @@
+const skills = {
+    1: [
+        {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "CSS",
+            level: 5,
+        },
+        {
+            title: "SASS",
+            level: 2,
+        },
+        {
+            title: "JavaScript",
+            level: 5,
+        },
+        {
+            title: "TypeScript",
+            level: 4,
+        },
+        {
+            title: "Tailwind CSS",
+            level: 2,
+        },
+        {
+            title: "React JS",
+            level: 6,
+        },
+        {
+            title: "Figma",
+            level: 2,
+        },
+    ],
+    2: [
+        {
+            title: "HTML",
+            level: 6,
+        },
+        {
+            title: "CSS",
+            level: 5,
+        },
+        {
+            title: "SASS",
+            level: 2,
+        },
+        {
+            title: "JavaScript",
+            level: 5,
+        },
+        {
+            title: "TypeScript",
+            level: 4,
+        },
+        {
+            title: "Tailwind CSS",
+            level: 2,
+        },
+        {
+            title: "React JS",
+            level: 6,
+        },
+        {
+            title: "Figma",
+            level: 2,
+        },
+    ],
+};
