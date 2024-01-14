1000a8afdd41f289798b3841b8205e3bf9abc55b
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-14 15:12:01 -0800

    started Projects page + project data + projectMenu

---
+++ OG-ReactPortfolio/src/projects/projectsData.jsx
@@ -0,0 +1,47 @@
+import projectOne from "../assets/project-1.png";
+import projectTwo from "../assets/project-2.png";
+import projectThree from "../assets/project-3.png";
+
+const projects = {
+    1: {
+        title: "SoMe Platform",
+        image: projectOne,
+        description: (
+            <>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed officia aut quaerat tempora et tenetur incidunt dolorem dignissimos distinctio!
+                </p>
+            </>
+        ),
+        github: "https://github.com/omargonz01",
+        demo: "https://netlify.com",
+    },
+    2: {
+        title: "Productivity App",
+        image: projectTwo,
+        description: (
+            <>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed officia aut quaerat tempora et tenetur incidunt dolorem dignissimos distinctio!
+                </p>
+            </>
+        ),
+        github: "https://github.com/omargonz01",
+        demo: "https://netlify.com",
+    },
+    3: {
+        title: "Food delivery App",
+        image: projectThree,
+        description: (
+            <>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed officia aut quaerat tempora et tenetur incidunt dolorem dignissimos distinctio!
+                </p>
+            </>
+        ),
+        github: "https://github.com/omargonz01",
+        demo: "https://netlify.com",
+    },
+};
+
+export default projects;
