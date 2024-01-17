b7a8bd631ccca6f5cc0f7b071338256a3d38d018
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-16 17:09:08 -0800

    begin initializing ContactMenujsx

---
+++ OG-ReactPortfolio/src/contact/ContactMenu.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import emailjs from "emailjs-com";
+import DOMPurify from "dompurify";
+
+export default function ContactMenu() {
+  const initialState = {
+    name:"",
+    email: "",
+    message: "",
+  };
+
+  const [formData, setFormData] = useState(initialState);
+  const [errors, setErrors] = useState({});
+  const [isLoading, setIsLoading] = useState(false);
+  const [isSent, setIsSent] = useState(false);
+
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setFormData((prevFormData) => ({
+        ...prevFormData,
+        [name]: value,
+
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const validateErrors = validateForm();
+    if (Object.keys(validateErrors).length > 0) {
+      setErrors(validateErrors);
+      return;
+    }
+    setIsLoading(true);
+
+    const { name, email, message } = formData;
+    const sanitizedData = {
+      name: "Name: " + DOMPurify.sanitize(name),
+      email: "Email: " + DOMPurify.sanitize(email),
+      name: "Message: " + DOMPurify.sanitize(message),
+    };
+
+    const serviceID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+    const templateID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+    const userID = process.env.REACT_APP_EMAILJS_USER_ID;
+
+    
+  };
+}
+  
