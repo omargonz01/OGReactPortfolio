45911f4f802ea82d8f5ed474e4bbeca3015d1072
Omar Gonzalez <gomarzalez@gmail.com>
2024-01-19 22:35:32 -0800

    update style + subheadingsData

--- OG-ReactPortfolio/src/about/subheadingsData.jsx
+++ OG-ReactPortfolio/src/about/subheadingsData.jsx
@@ -5,8 +5,7 @@ const subheadingsData = {
             content: (
                 <>
                     <p>
-                        I am a software engineer with a passion for building
-                        beautiful, responsive, and accessible websites.
+                    Fueled by the thrill of untangling complex problems and crafting elegant solutions, I'm a Full-Stack Software Engineer ignited by Coding Temple's exemplary team and guidance. Armed with Python, Flask, JS, React,  and a toolbox of other modern technologies, I thrive in collaborative environments, honed by seasoned devs with 12+ years of experience. Watch me code with critical thinking, adaptability, and unwavering persistence – Let's build something epic together! Send me a message ✉ :) 📨💌
                     </p>
                 </>
             ),
@@ -14,23 +13,23 @@ const subheadingsData = {
     ],
     2: [
         {
-            title: "Coding Temple Software Engineer",
+            title: "Coding Temple",
             content: (
                 <>
                     <p>
-                        I am a software engineer with a passion for building
-                        beautiful, responsive, and accessible websites.
+                    <u>FS Software Engineering Bootcamp:</u><br></br>
+                    Was able to accelerate my career path at Coding Temple's Full Stack software engineering bootcamp, emerging as a passionate and versatile software engineer. <br></br>Coding Temple wasn't just a bootcamp, it was a catalyst for my creativity. I poured my heart and soul into building Degen Zone and This Portfolio, two passion projects that demonstrate my mastery of HTML + CSS + Python + Flask + JavaScript + TypeScript + React.
                     </p>
                 </>
             ),
         },
         {
-            title: "Other Ed",
+            title: "Boolean",
             content: (
                 <>
                     <p>
-                        I am a software engineer with a passion for building
-                        beautiful, responsive, and accessible websites.
+                    <u>Coding Bootcamp:</u><br></br>
+                    Deepened my JavaScript skills alongside seasoned devs (combined 30+ years experience) during Boolean's coding program. Collaborated daily to refine coding practices, explore industry trends, and connected with experienced devs (10-12+ years each!)
                     </p>
                 </>
             ),
@@ -42,30 +41,40 @@ const subheadingsData = {
             content: (
                 <>
                     <p>
-                        I am a software engineer with a passion for building
-                        beautiful, responsive, and accessible websites.
+                    Ignited my passion for code mastering Python, Flask, React, Javascript and more. Now, I craft user-centric interfaces and solve problems, bringing ideas to life in a pixel-perfect way.<br></br>Data whisperer by day, UI/UX magician by night. My customer-focused approach ensure delightful experiences for every user, exceeding expectations on every line of code... ;)
                     </p>
                 </>
             ),
         },
         {
-            title: "2020 UX Designer",
+            title: "2021 Account Manager",
             content: (
                 <>
                     <p>
-                        I am a software engineer with a passion for building
-                        beautiful, responsive, and accessible websites.
+                    Transformed insurance sales, growing customer base by 10% and retention by 15%. Conflict resolution expert and bilingual bridgebuilder, I ensured customer satisfaction through exceptional service and targeted marketing.<br></br>
+                    A true champion of customer loyalty. From streamlining lead intake to resolving 500+ complaints, I thrived in a fast-paced environment, exceeding expectations and delivering results.
                     </p>
                 </>
             ),
         },
         {
-            title: "2019 IT Consultant",
+            title: "2017 Agency Owner",
             content: (
                 <>
                     <p>
-                        I am a software engineer with a passion for building
-                        beautiful, responsive, and accessible websites.
+                    Launched my own insurance agency, merging with a top agent and expanding market share. Strong collaboration and management skills fueled my success, leading diverse teams and managing the needs of 1000+ clients. <br></br>
+                    Entrepreneur at heart, problem solver by nature. I built an agency from scratch, navigating complex challenges and exceeding goals. Now, I leverage that experience to build beautiful and impactful software solutions.
+                    </p>
+                </>
+            ),
+        },
+        {
+            title: "2015 Agent",
+            content: (
+                <>
+                    <p>
+                    Laid the foundation for my customer-centric approach and communication skills as an insurance agent. Building trust and relationships with diverse clients fueled my passion for understanding user needs and delivering solutions. <br></br>
+                    Every journey begins with a step. My first steps in the insurance world honed my empathy and ability to connect with people. Now, I apply those skills to building engaging and accessible web experiences.
                     </p>
                 </>
             ),
--- OG-ReactPortfolio/src/playerStats/PlayerStats.jsx
+++ OG-ReactPortfolio/src/playerStats/PlayerStats.jsx
@@ -4,7 +4,7 @@ import "../styles/playerStats.css";
 const PlayerStats = () => {
     return (
         <div id="playerStats">
-            <h1>OMAR GONZALEZ LEVEL 69</h1>
+            <h1>OMAR GONZALEZ LEVEL 68</h1>
             <div id="playerStats_lines"> 
                 <span id="playerStats_lines_thick"></span>
                 <span id="playerStats_lines_thin"></span>
